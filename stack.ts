85089d3bca584f7e6ad5319549a2c7e7f39e918e
WenheLI <wl1508@nyu.edu>
2019-12-03 20:33:12 +0800

    add stacked

--- stack.ts
+++ stack.ts
@@ -12,12 +12,67 @@ const padding = 90;
 const yearParser = d3.timeParse('%Y');
 const currScale = [1980, 1995];
 
+const colorScheme = [
+    "#4c5270",
+    "#C26DBC",
+    "#36EEE0",
+    "#EDC1BB",
+    "#0C6980",
+    "#C4DBE0",
+    "#CE2380",
+    "#2D3A3E",
+    "#DEE2EC",
+    "#8C756A",
+    "#EEB5EB",
+    "#C6D830",
+    "#A3EBB1",
+    "#AE8B70",
+    "#543855",
+    "#81B622",
+    "#21B6A8",
+    "#73D9F0",
+    "#F7B301",
+    "#EB773E",
+    "#B7A1A6",
+    "#854B5B"
+]
+
 const makeClip = (viz, target) => {
     return viz.append("clipPath")
             .attr("id", `clip-${target}`)
             .append("rect")
             .attr("width", 0)
             .attr("height", h);
+}
+
+const makeLabel = (labels: any, i: number, x: number, y: number, text: string, isOpa=true) => {
+    const dot = labels.append('g')
+            .attr('class', 'dots')
+            .attr('id', `label-${text}`)
+            .on('mouseenter', () => {
+                d3.selectAll('.layer').transition().duration(500).style('opacity', .1);
+                if (text === 'atari') d3.select(`.layer-2600`).transition().duration(500).style('opacity', 1)
+                d3.select(`.layer-${text}`).transition().duration(500).style('opacity', 1)
+            })
+            .on('mouseleave', () => {
+                d3.selectAll('.layer').transition().duration(500).style('opacity', 1);
+            })
+            .style('opacity', isOpa ? 0 : 1);
+
+
+        dot.append('circle')
+        .attr('cx', x)
+        .attr('cy', y)
+        .attr('r', 5)
+        .style('fill', colorScheme[i] )
+
+        dot.append('text')
+        .attr('x', x + 10)
+        .attr('y', y)
+        .text(text)
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
+        .style('fill', colorScheme[i] )
 }
 
 const makeUpdate = (xScale, yScale, selectData, xAxisGroup, yAxisGroup, areaChart, area) => {
@@ -47,7 +102,9 @@ const main = async () => {
     const viz = d3.select('#display').append('svg')
         .style('width', w)
         .style('height', h);
-
+    const labels = viz.append('g')
+        .attr('class', 'labels')
+        .style('transform', `translate(${10}px, ${5}px)`)
     const {
         rawData,
         data
@@ -63,8 +120,8 @@ const main = async () => {
         });
 
     const salesData = getYearSales(years, data);
-    let selectYears = years.filter((it) => parseInt(it) <= 2017);
-    let selectData = salesData.filter((it) => it.year.getFullYear() <= 2017);
+    let selectYears = years.filter((it) => parseInt(it) <= 2003);
+    let selectData = salesData.filter((it) => it.year.getFullYear() <= 2003);
     let allPlatforms = getAllplatforms(selectData);
 
     const flattenData = salesData.map((it) => {
@@ -84,8 +141,7 @@ const main = async () => {
     console.log(allPlatforms)
     let stackData = d3.stack()
         .keys(allPlatforms)(flattenData);
-
-    console.log(stackData.map((it) => it.key))
+    const colorScale = d3.scaleSequential().domain([0,allPlatforms.length - 1]).interpolator(d3.interpolateTurbo);
     let yMax = d3.max(selectData, (d) => d.sales);
     const xDomain = d3.extent(selectData, (d) => d.year);
     const yDomain = [0, yMax];
@@ -107,8 +163,6 @@ const main = async () => {
         .attr("transform", "translate(" + (padding / 2) + ",0)");
     yAxisGroup.call(yAxis);
 
-    const colorScale = d3.scaleOrdinal().domain(allPlatforms)
-        .range(d3.schemeSet2)
 
     const clip = viz.append("defs").append("svg:clipPath")
         .attr("id", "clip")
@@ -128,15 +182,11 @@ const main = async () => {
         .data(stackData)
         .enter()
         .append('path')
-        .style('fill', (d) => {
-            return colorScale(d.key)
+        .style('fill', (d, i) => {
+            return colorScheme[i]
         })
         .attr("clip-path", (d) => `url(#clip-${d.key})`)
-        .attr('class', (d) => `layer-${d.key}`)
-        .attr('class', (d) => {
-            makeClip(viz, d.key)
-        })
-        .attr('class', 'layer')
+        .attr('class', (d) => `layer-${d.key} ${makeClip(viz, d.key)} layer`)
         .attr('d', area);
     const lineMaker = d3.line()
         .x((d) => xScale(d.year))
@@ -159,10 +209,34 @@ const main = async () => {
         globalLine.attr("stroke-dasharray", length + " " + length)
         .attr("stroke-dashoffset", length);
 
+    makeLabel(labels, 0, 10, 10, 'atari');
+    makeLabel(labels, 1, 80, 10, 'NES');
+    makeLabel(labels, 2, 150, 10, 'SNES');
+    makeLabel(labels, 3, 230, 10, 'GB');
+    let x = 280;
+    let y = 10;
+    [ "PC", "DS", "GEN", "GG", "SCD", "NG", "PS", "SAT" ].forEach((it, i) => {
+        makeLabel(labels, i+4, x, y, it);
+        x += 80;
+        if (x > 800) {
+            x = 10;
+            y += 30;
+        }
+    });
+    ["3DO", "TG16", "N64", "PCFX", "DC", "WS", "PS2", "XB", "GBA", "GC"].forEach((it, i) => {
+        makeLabel(labels, i+12, x, y, it);
+        x += 80;
+        if (x > 800) {
+            x = 10;
+            y += 30;
+        }
+    });
+    const opacityScale = d3.scaleLinear().domain([0, window.innerHeight]).range([0, 1]);
 
     const column = document.getElementById('column');
     column.addEventListener('scroll', (e) => {
         const pageNum = Math.floor(column.scrollTop / window.innerHeight);
+        let op = opacityScale(column.scrollTop % window.innerHeight);
 
         switch (pageNum) {
             case 0:
@@ -173,6 +247,7 @@ const main = async () => {
                 const atariScale = d3.scaleLinear().domain([0, window.innerHeight]).range([0, xScale(yearParser('1992'))]);
                 const atariClip = d3.select('#clip-2600 rect')
                 atariClip.attr("width", atariScale(column.scrollTop % window.innerHeight));
+                d3.select('#label-atari').style('opacity', opacityScale(column.scrollTop % window.innerHeight));
                 // d3.select('#label-atari').style('opacity', opacityScale(column.scrollTop % window.innerHeight));
     
                 break;
@@ -184,16 +259,20 @@ const main = async () => {
                 nesClip.attr("width", nesScale(column.scrollTop % window.innerHeight));
                 snesClip.attr("width", nesScale(column.scrollTop % window.innerHeight));
                 gbClip.attr("width", nesScale(column.scrollTop % window.innerHeight));
+                d3.select('#label-nes').style('opacity', op);
+                d3.select('#label-snes').style('opacity', op);
+                d3.select('#label-gb').style('opacity', op);
                 break;
             case 3:
-                const other90 = d3.scaleLinear().domain([0, window.innerHeight]).range([ xScale(yearParser('1982')), xScale(yearParser('1995'))]);
+                const other90 = d3.scaleLinear().domain([0, window.innerHeight]).range([ xScale(yearParser('1988')), xScale(yearParser('1995'))]);
                 [ "PC", "DS", "GEN", "GG", "SCD", "NG", "PS", "SAT" ].forEach((it) => {
                     const clip = d3.select(`#clip-${it} rect`);
                     clip.attr("width", other90(column.scrollTop % window.innerHeight));
+                    d3.select(`#label-${it}`).style('opacity', op);
                 })
                 break;
             case 4:
-                const trans9010 = d3.scaleLinear().domain([0, window.innerHeight]).range([ 1995, 2005]);
+                const trans9010 = d3.scaleLinear().domain([0, window.innerHeight]).range([ 1995, 2004]);
                 const year =Math.floor(trans9010(column.scrollTop % window.innerHeight));
                 currScale[1] = year;
         
@@ -203,12 +282,14 @@ const main = async () => {
                 l.attr('d', lineMaker(temp));
                 break;
             case 5:
-                    const scale1 = d3.scaleLinear().domain([0, window.innerHeight]).range([ xScale(yearParser('1995')), xScale(yearParser('2005'))]);
+                const scale1 = d3.scaleLinear().domain([0, window.innerHeight]).range([ xScale(yearParser('1995')), xScale(yearParser('2004'))]);
 
-                    const platforms = ["3DO", "TG16", "N64", "PCFX", "DC", "WS", "PS2", "XB", "GBA", "GC"];
-                    platforms.forEach((it) => {
-                        d3.select(`#clip-${it} rect`).attr('width', scale1(column.scrollTop % window.innerHeight))
-                    })
+                const platforms = ["3DO", "TG16", "N64", "PCFX", "DC", "WS", "PS2", "XB", "GBA", "GC"];
+                platforms.forEach((it) => {
+                    d3.select(`#clip-${it} rect`).attr('width', scale1(column.scrollTop % window.innerHeight))
+                    d3.select(`#label-${it}`).style('opacity', op);
+                });
+                break;
             default:
                 console.log(pageNum)
         }
@@ -220,7 +301,6 @@ const main = async () => {
     //                         .call(d3.axisBottom(xScale));
     //                         areaChart.selectAll('path')
     //                         .attr('d', area)
-    // })
 
 }
 
